bc8cbb9ec7ca3586a0c65de87bef581e31457d8a
Eduardo Espinoza <42099005+tttedu304@users.noreply.github.com>
2019-04-27 16:42:50 -0500

    Added basic routes, search by id, search by name, full code list, creating add...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,37 @@
 const  { postCode, removeCode, fetchCode} = require("./utils/index");
 const { connect } = require("./db/connection");
 const { mongouri } = require("../config");
+const Code = require("./db/schema/codeList");
+const express = require("express");
+let app = express();
 
 connect(mongouri);
 
-console.log("Started!");
+app.get("/", async(req, res) => {
+    res.send('Hello world!');
+});
+
+app.get("/codes", async(req, res) => {
+    const Codes = await Code.find({});
+        res.send(Codes);
+});
+
+app.get("/codes/:id", async(req, res) => {
+   const id = req.params.id;
+   const code = await Code.find({_id: id});
+   res.send(code);
+});
+
+app.get("/codes/search/:name", async(req, res) => {
+   const name = req.params.name;
+   const code = await Code.find({name: name});
+   res.send(code);
+});
+
+app.post("/codes/add", async(req, res) => {
+
+});
+
+app.listen(3000);
+
+console.log(`Started!\nListening in port: 3000`);
